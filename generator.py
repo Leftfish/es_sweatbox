42760d27ac486e03d2f6bc4af4cc5497494f2f53
Leftfish <bohdan@widla.net>
2025-08-05 19:03:45 +0200

    Add scaffolding for simdata import from JSON. Minor refactors.

--- generator.py
+++ generator.py
@@ -13,12 +13,18 @@ from simdata import pseudopilot_data, airport_alt, runway_data, holding_data, co
                     inbound_spawns, requested_altitude_departures, requested_altitude_arrivals, \
                     initial_pseudopilot, arrivals_star_waypoints
 
-from defaults import DEFAULT_HDG, DEFAULT_SPAWN, DEFAULT_TAXI_SPEED, DEFAULT_TAXIWAY_USAGE, DEFAULT_OBJECT_EXTENT, \
-                    DEFAULT_REQ_ALT_DEPARTURE, DEFAULT_REQ_ALT_ARRIVAL, DEFAULT_WAVE_INTERVAL
+from defaults import DEFAULT_HDG, DEFAULT_SPAWN, DEFAULT_TAXI_SPEED, DEFAULT_TAXIWAY_USAGE, \
+                     DEFAULT_OBJECT_EXTENT, DEFAULT_REQ_ALT_DEPARTURE, DEFAULT_REQ_ALT_ARRIVAL, \
+                     DEFAULT_WAVE_INTERVAL, DEFAULT_WAVE_START
 
 from templates import HOLDING_TEMPLATE, POSITION_TEMPLATE, FPL_TEMPLATE, PSEUDOPILOT_TEMPLATE, \
-                    SIMDATA_TEMPLATE, ROUTE_TEMPLATE, REQUALT_TEMPLATE, FLIGHT_TEMPLATE, SCENARIO_TEMPLATE, RUNWAY_TEMPLATE
+                      SIMDATA_TEMPLATE, ROUTE_TEMPLATE, REQUALT_TEMPLATE, FLIGHT_TEMPLATE, \
+                      SCENARIO_TEMPLATE, RUNWAY_TEMPLATE
 
+
+def import_simdata(simdata_path):
+    with open(simdata_path, 'r', encoding='utf-8') as simdata_file:
+        return json.load(simdata_file)
 
 def generate_runways(runway_data):
     runways = []
@@ -46,7 +52,7 @@ def transform_heading(flight_data):
 
 def get_spawn_coordinates(flight_data):
     if not flight_data['latitude'] or not flight_data['longitude']:
-        tma_boundary = flight_data['fpl_route'].split()[-1]  # assumes that last wpt in the route is the TMA boundary
+        tma_boundary = flight_data['fpl_route'].split()[-1]  # last wpt is the TMA boundary
         spawn_latitude, spawn_longitude = inbound_spawns.get(tma_boundary, DEFAULT_SPAWN)
     else:
         spawn_latitude, spawn_longitude = flight_data['latitude'], flight_data['longitude']
@@ -55,17 +61,18 @@ def get_spawn_coordinates(flight_data):
 def generate_position_data(flight_data):
     spawn_latitude, spawn_longitude = get_spawn_coordinates(flight_data)
     transformed_heading = transform_heading(flight_data)
-    position = POSITION_TEMPLATE.format(flight_data['transponder'], flight_data['callsign'], flight_data['squawk'],
-                                        spawn_latitude, spawn_longitude, flight_data['altitude'], transformed_heading)
-    return position
+    return POSITION_TEMPLATE.format(flight_data['transponder'], flight_data['callsign'], 
+                                    flight_data['squawk'], spawn_latitude, spawn_longitude,
+                                    flight_data['altitude'], transformed_heading)
 
 def generate_fpl_data(flight_data):
-    fpl_data = FPL_TEMPLATE.format(flight_data['callsign'], flight_data['flight_plan_type'], flight_data['aircraft_type'],
-                                    flight_data['true_air_speed'], flight_data['origin_airport'], flight_data['departure_time_est'],
-                                    flight_data['departure_time_act'], flight_data['final_cruising_altitude'], flight_data['destination_airport'],
-                                    flight_data['hrs_en_route'], flight_data['mins_en_route'], flight_data['hrs_fuel'],
-                                    flight_data['mins_fuel'], flight_data['alternate_airport'], flight_data['remarks'], flight_data['fpl_route'])
-    return fpl_data
+    return FPL_TEMPLATE.format(flight_data['callsign'], flight_data['flight_plan_type'],
+                               flight_data['aircraft_type'], flight_data['true_air_speed'],
+                               flight_data['origin_airport'], flight_data['departure_time_est'],
+                               flight_data['departure_time_act'], flight_data['final_cruising_altitude'],
+                               flight_data['destination_airport'], flight_data['hrs_en_route'],
+                               flight_data['mins_en_route'], flight_data['hrs_fuel'], flight_data['mins_fuel'],
+                               flight_data['alternate_airport'], flight_data['remarks'], flight_data['fpl_route'])
 
 def generate_simdata(flight_data):
     taxi_speed = flight_data['max_taxi_speed'] if flight_data['max_taxi_speed'] else DEFAULT_TAXI_SPEED
@@ -104,8 +111,8 @@ def generate_reqalt(flight_data, sid_waypoints='', star_waypoints=''):
 
     return reqalt
 
-def generate_flight(flight_data, pseudopilot_data, initial_pseudopilot, \
-                    sid_waypoints = '', star_waypoints = '', start = 0):
+def generate_single_flight(flight_data, pseudopilot_data, initial_pseudopilot, start, \
+                    sid_waypoints = '', star_waypoints = ''):
     position = generate_position_data(flight_data)
     flight_plan = generate_fpl_data(flight_data)
     simdata = generate_simdata(flight_data)
@@ -118,31 +125,28 @@ def generate_flight(flight_data, pseudopilot_data, initial_pseudopilot, \
 
     return flight
 
-def generate_flights_from_json(path):
-    with open(path, 'r', encoding='utf-8') as flights_file:
+def generate_flights_from_json(path_flights_file):
+    with open(path_flights_file, 'r', encoding='utf-8') as flights_file:
         all_flights_data = json.load(flights_file)
 
     flights = []
-    start = 0
+    start = DEFAULT_WAVE_START
 
     for flight_data in all_flights_data:
         destination_rwy = flight_data.get('destination_rwy', None)
         tma_boundary = flight_data['fpl_route'].split()[-1]
         star_waypoints = arrivals_star_waypoints[destination_rwy][tma_boundary]
-        flight = generate_flight(flight_data, pseudopilot_data, initial_pseudopilot, star_waypoints=star_waypoints, start=start)
-
+        flight = generate_single_flight(flight_data, pseudopilot_data, initial_pseudopilot, start, star_waypoints=star_waypoints)
         flights.append(flight)
 
         start += DEFAULT_WAVE_INTERVAL
 
     return flights
 
-
-def make_test_scenario(path, runway_data=runway_data, holding_data=holding_data, controller_data=controller_data, pseudopilot_data=pseudopilot_data):
+def make_test_scenario(path_flights_file, runway_data, holding_data, controller_data, pseudopilot_data):
     runways, holdings, controllers = generate_runways(runway_data), generate_holdings(holding_data), generate_controllers(controller_data, pseudopilot_data)
-    flight = '\n'.join(generate_flights_from_json(path))
+    flight = '\n'.join(generate_flights_from_json(path_flights_file))
     return SCENARIO_TEMPLATE.format(pseudopilot_data, airport_alt, runways, holdings, controllers, flight)
 
 with open('test_scenario.txt', 'w', encoding='utf-8') as scenario_file:
-    scen = make_test_scenario(path='flights.json')
-    scenario_file.write(scen)
+    scenario_file.write(make_test_scenario('flights.json', runway_data, holding_data, controller_data, pseudopilot_data))
